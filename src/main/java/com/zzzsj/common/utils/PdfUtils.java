494f3f6b0d2d6ffeff5a8498f67ee2efb8932e5a
yuzijie <792826371@qq.com>
2020-04-02 11:08:49 +0800

    pdfUtil工具类编写

--- src/main/java/com/zzzsj/common/utils/PdfUtils.java
+++ src/main/java/com/zzzsj/common/utils/PdfUtils.java
@@ -1,53 +1,55 @@
 package com.zzzsj.common.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.Map;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+import com.zzzsj.common.enums.ErrorCode;
+import com.zzzsj.common.exception.BizException;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by wangpeng on 2018/02/01.
  */
 public class PdfUtils {
-    // 利用模板生成pdf
-    public static void pdfout(Map<String,Object> o) {
+    /**
+     *  利用模板生成pdf
+     */
+    public static void pdfout(Map<String,Object> o, HttpServletResponse response) {
         // 模板路径
         String templatePath = "/Users/yyyzj/Desktop/aaa.pdf";
         // 生成的新文件路径
         String newPDFPath = "/Users/yyyzj/Desktop/bbb.pdf";
 
         PdfReader reader;
-        FileOutputStream out;
+        //FileOutputStream out;
         ByteArrayOutputStream bos;
         PdfStamper stamper;
-        //OutputStream out = null;
+        OutputStream out = null;
         try {
             //中文字体
             BaseFont bfChinese = BaseFont.createFont( "STSongStd-Light" ,"UniGB-UCS2-H",BaseFont.NOT_EMBEDDED);
             Font font = new Font(bfChinese, 12,Font.NORMAL);
 
-//            // 输出流
-//            response.setContentType("application/pdf");
-//            response.setHeader("Content-Disposition",
-//                    "attachment;fileName=" + URLEncoder.encode(datemap.get("billId") + ".pdf", "UTF-8"));
-//            out = new BufferedOutputStream(response.getOutputStream());
-            out = new FileOutputStream(newPDFPath);// 输出流
-            reader = new PdfReader(templatePath);// 读取pdf模板
+            // 输出流
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition",
+                    "attachment;fileName=" + URLEncoder.encode("bbb" + ".pdf", "UTF-8"));
+            out = new BufferedOutputStream(response.getOutputStream());
+            /**
+             * 读取pdf模板
+             */
+            reader = new PdfReader(templatePath);
             bos = new ByteArrayOutputStream();
             stamper = new PdfStamper(reader, bos);
             AcroFields form = stamper.getAcroFields();
             //文字类的内容处理
             Map<String,String> datemap = (Map<String,String>)o.get("datemap");
-            //form.addSubstitutionFont(bf);
             for(String key : datemap.keySet()){
                 String value = datemap.get(key);
-//                if (key.equals("ball")) {
-//                    form.setField(key,value);
-//                }
                 form.setField(key,value);
             }
             //图片类的内容处理
@@ -69,10 +71,12 @@ public class PdfUtils {
                 image.setAbsolutePosition(x, y);
                 under.addImage(image);
             }
-            stamper.setFormFlattening(true);// 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
+            /**
+             * 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
+             */
+            stamper.setFormFlattening(true);
             stamper.close();
             Document doc = new Document();
-            //Font font = new Font(bf, 32);
             PdfCopy copy = new PdfCopy(doc, out);
             doc.open();
             PdfImportedPage importPage = copy.getImportedPage(new PdfReader(bos.toByteArray()), 1);
@@ -80,26 +84,12 @@ public class PdfUtils {
             doc.close();
 
         } catch (IOException e) {
-            System.out.println(e);
+            throw new BizException(ErrorCode.SYS_EXCEPTION.getCode(), ErrorCode.SYS_EXCEPTION.getMsg());
         } catch (DocumentException e) {
-            System.out.println(e);
+            throw new BizException(ErrorCode.SYS_EXCEPTION.getCode(), ErrorCode.SYS_EXCEPTION.getMsg());
         }
 
     }
 
-    public static void main(String[] args) {
-        Map<String,String> map = new HashMap();
-        map.put("name","张三");
-        map.put("creatdate","今天");
-        map.put("weather","晴");
-        map.put("sports","踢球");
-        map.put("ball","Off");
-        Map<String,String> map2 = new HashMap();
-        map2.put("img","/Users/yyyzj/Pictures/376189334869.jpg");
 
-        Map<String,Object> o=new HashMap();
-        o.put("datemap",map);
-        o.put("imgmap",map2);
-        pdfout(o);
-    }
 }
--- src/main/java/com/zzzsj/controller/StudentController.java
+++ src/main/java/com/zzzsj/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.zzzsj.controller;
 
+import com.zzzsj.common.utils.PdfUtils;
 import com.zzzsj.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 79282
@@ -27,4 +30,21 @@ public class StudentController {
     public void importStudent(MultipartFile file) {
         studentService.importStudent(file);
     }
+
+    @RequestMapping("/pdf")
+    public void pdfDownload(HttpServletResponse response) {
+        Map<String,String> map = new HashMap();
+        map.put("name","张三");
+        map.put("creatdate","今天");
+        map.put("weather","晴");
+        map.put("sports","踢球");
+        map.put("ball","On");
+        Map<String,String> map2 = new HashMap();
+        map2.put("img","/Users/yyyzj/Pictures/376189334869.jpg");
+        Map<String,Object> o=new HashMap();
+        o.put("datemap",map);
+        o.put("imgmap",map2);
+        PdfUtils.pdfout(o,response);
+    }
+
 }
